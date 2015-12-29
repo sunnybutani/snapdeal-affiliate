e18e1d37434ebb0543ad8b978416c1d8567802b6
sunnybutani <sunnybutani@gmail.com>
2015-12-29 23:28:57 +0530

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,8 @@
+# name: snapdeal-affiliate
+# version: 0.1
+# authors: Sunny Butani
+# url: https://github.com/sunnybutani/snapdeal-affiliate
+
+enabled_site_setting :affiliate_enabled
+
+register_asset "javascripts/affiliate_dialect.js", :server_side
