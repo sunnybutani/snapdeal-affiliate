5b00cbf5839b9a8486bdcfc51a6dd5dbd605303c
sunnybutani <sunnybutani@gmail.com>
2015-12-29 23:43:40 +0530

    Create affiliate_dialect.js

---
+++ assets/javascripts/affiliate_dialect.js
@@ -0,0 +1,26 @@
+(function() {
+
+  var SNAPDEAL_LINK_REGEX = /((?:https?:)?(?:\/\/)?(?:www\.)?snapdeal\.[^\b\s"'<>()]+)/ig;
+  var SNAPDEAL_DOMAIN_EXTRACTOR_REGEX = /snapdeal\.([^\?\/]{2,})/i;
+  var SNAPDEAL_ASIN_EXTRACTOR_REGEX = /\/([A-Z0-9]{10})(?:[\?\/%]|$)/i;
+
+  Discourse.Dialect.addPreProcessor(function(text) {
+    if (Discourse.SiteSettings.affiliate_enabled) {
+      text = text.replace(SNAPDEAL_LINK_REGEX, function(href) {
+        if (SNAPDEAL_DOMAIN_EXTRACTOR_REGEX.test(href)) {
+          var domain = SNAPDEAL_DOMAIN_EXTRACTOR_REGEX.exec(href)[1];
+          if (SNAPDEAL_ASIN_EXTRACTOR_REGEX.test(href)) {
+            var asin = SNAPDEAL_ASIN_EXTRACTOR_REGEX.exec(href)[1];
+            href = "https://www.snapdeal." + domain + "/dp/" + asin;
+            if (Discourse.SiteSettings.affiliate_snapdeal_tag.length > 0) {
+              href += "?utm_source=aff_prog&utm_campaign=afts&offer_id=17&aff_id=" + Discourse.SiteSettings.affiliate_snapdeal_tag;
+            }
+          }
+        }
+        return href;
+      });
+    }
+    return text;
+  });
+
+})();
